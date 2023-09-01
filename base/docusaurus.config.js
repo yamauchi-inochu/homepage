6c80586b5147d6d8f44522728c58e84124ca7129
Hiroyuki Yamauchi <h.yamauchi@csis.u-tokyo.ac.jp>
2023-09-02 00:47:28 +0900

    up

--- base/docusaurus.config.js
+++ base/docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/yamauchi-inochu/home/base',
+            'https://github.com/yamauchi-inochu/homepage/tree/main/base',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/yamauchi-inochu/home/base',
+            'https://github.com/yamauchi-inochu/homepage/tree/main/base',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
